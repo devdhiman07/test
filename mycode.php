f0e1b824087dde0cafa75ffbe8fafdb520c67bf8
Devinder <d.dhiman@syncapse.com>
2014-03-13 16:58:57 +0530

    new array

--- mycode.php
+++ mycode.php
@@ -4,6 +4,11 @@ echo "hello world !"
 
 echo "new code inserted from development branch";
 
-echo "code need to be added";
+$arr = array('0'=>'A', '1'=>'B');
+$arr2 = array('0'=>'C', '1'=>'D');
+
+print"<pre>";
+print_r($arr);
+
 
 ?>
